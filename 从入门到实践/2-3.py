124c3e7085ad41e728ec86ff8cfeb164e36e3f54
cogito0823 <asd754032908@gmail.com>
2019-11-21 17:11:23 +0800

    add 2-3.py

---
+++ 从入门到实践/2-3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#########################################################################
+# File Name: 2-3.py
+# Created on : 2019-11-21 16:58:37
+# Author: cogito0823
+# Last Modified: 2019-11-21 16:58:37
+# Description: 个性化消息： 将用户的姓名存到一个变量中，并向该用户显示一条消息。显示的消息应非常简单，如“Hello Eric, would you like to learn some Python today?”。
+#########################################################################
+
+name = input('Username: ')
+print('Hello ' + name + ',would you like to learn some Python today?')
+
