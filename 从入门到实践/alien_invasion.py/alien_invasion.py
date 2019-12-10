dc393a170732897fed57c622521fd73a4bf9f324
cogito0823 <asd754032908@gmail.com>
2019-12-10 20:42:52 +0800

    add

---
+++ 从入门到实践/alien_invasion.py/alien_invasion.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#########################################################################
+# File Name: alien_invasion.py
+# Created on : 2019-11-21 18:43:15
+# Author: cogito0823
+# Last Modified: 2019-11-21 18:43:15
+# Description:
+#########################################################################
+
+import sys
+import pygame
+def run_game():
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+    # 开始游戏的主循环
+    while True:
+        # 监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+run_game()
