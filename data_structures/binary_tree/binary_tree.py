42680206ffdc31f0b03add9f7383848d81d6e514
cogito0823 <asd754032908@gmail.com>
2020-03-14 11:56:56 +0800

    git add .

--- data_structures/binary_tree/binary_tree.py
+++ data_structures/binary_tree/binary_tree.py
@@ -1,9 +1,24 @@
 class Node():
+    """节点类
+    """
     def __init__(self,data):
+        """初始化
+        
+        Args:
+            data (var): 
+        """
         self.data = data
         self.left_child = None
         self.right_child = None
 def create_binary_tree(tree_list):
+    """通过一个数组创建二叉树
+    
+    Args:
+        tree_list ( ):  
+    
+    Returns:
+        Node:  
+    """
     data = tree_list.pop(0)
     if data:
         node = Node(data)
@@ -13,16 +28,31 @@ def create_binary_tree(tree_list):
     else:
         return None
 def pre_order_Traveral(node):
+    """先序遍历
+    
+    Args:
+        node (Node): 二叉树
+    """
     if node:
         print(node.data)
         pre_order_Traveral(node.left_child)
         pre_order_Traveral(node.right_child)
 def in_order_Traveral(node):
+    """中序遍历
+    
+    Args:
+        node (Node): 二叉树
+    """
     if node:
         in_order_Traveral(node.left_child)
         print(node.data)
         in_order_Traveral(node.right_child)
 def post_order_Traveral(node):
+    """后序遍历
+    
+    Args:
+        node (Node): 二叉树
+    """
     if node:
         post_order_Traveral(node.left_child)
         post_order_Traveral(node.right_child)
