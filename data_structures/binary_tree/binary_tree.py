48f8ea1feb55dc515f4325b30ef55ff5174fd65e
cogito0823 <asd754032908@gmail.com>
2020-03-14 10:02:24 +0800

    add binary_tree.py

---
+++ data_structures/binary_tree/binary_tree.py
@@ -0,0 +1,29 @@
+class Node():
+    def __init__(self,data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+def create_binary_tree(tree_list):
+    data = tree_lsit.pop(0)
+    if data is None:
+        return None
+    else:
+        node = Node(data)
+        node.left_child = create_binary_tree(tree_list)
+        node.right_child = create_binary_tree(tree_list)
+        return node
+def pre_order_Traveral(node):
+    if node:
+        print(node.data)
+        pre_order_Traveral(node.left_child)
+        pre_order_Traveral(node.right_child)
+def in_order_Traveral(node):
+    if node:
+        in_order_Traveral(node.left_child)
+        print(node.data)
+        in_order_Traveral(node.right_child)
+def post_order_Traveral(node):
+    if node:
+        post_order_Traveral(node.left_child)
+        post_order_Traveral(node.right_child)
+        print(node.data)
