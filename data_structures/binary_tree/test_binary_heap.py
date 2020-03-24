fb4d6d38fe8172df813f795b105fc8a0b9e0990c
cogito0823 <asd754032908@gmail.com>
2020-03-25 01:04:48 +0800

    feat(data_structures/binary_tree/binary_heap.py): 添加对堆排序的测试

--- data_structures/binary_tree/test_binary_heap.py
+++ data_structures/binary_tree/test_binary_heap.py
@@ -17,23 +17,23 @@ class BheapTestCase(unittest.TestCase):
     
     def setUp(self):
         """setIp"""
-        self.array_full1 = [0,1,2,3,4,5,6,7]
-        self.array_full2 = [0,1,2,3,4,5,6]
-        self.array_up1 = [1,2,3,4,5,6,7,0]
-        self.array_up2 = [1,2,3,4,5,6,0]
-        self.array_down1 = [7,0,1,2,3,4,5,6]
-        self.array_down2 = [7,1,2,3,4,5,6]
-        self.array_build = [4,3,7,0,2,6,5,1]
-        self.array_one_element = [1]
-        self.array_emp = []
-        self.array_None = None
+        # [0,1,2,3,4,5,6,7] = [0,1,2,3,4,5,6,7]
+        # [0,1,2,3,4,5,6] = [0,1,2,3,4,5,6]
+        # [1,2,3,4,5,6,7,0] = [1,2,3,4,5,6,7,0]
+        # [1,2,3,4,5,6,0] = [1,2,3,4,5,6,0]
+        # [7,0,1,2,3,4,5,6] = [7,0,1,2,3,4,5,6]
+        # [7,1,2,3,4,5,6] = [7,1,2,3,4,5,6]
+        # [4,3,7,0,2,6,5,1] = [4,3,7,0,2,6,5,1]
+        # [1] = [1]
+        # [] = []
+        # None = None
     def test_up_adjust(self):
         """测试插入操作"""
-        result_up1 = binary_heap.up_adjust(self.array_up1)
-        result_up2 = binary_heap.up_adjust(self.array_up2)
-        result_one_element = binary_heap.up_adjust(self.array_one_element)
-        result_emp = binary_heap.up_adjust(self.array_emp)
-        result_None = binary_heap.up_adjust(self.array_None)
+        result_up1 = binary_heap.up_adjust([1,2,3,4,5,6,7,0])
+        result_up2 = binary_heap.up_adjust([1,2,3,4,5,6,0])
+        result_one_element = binary_heap.up_adjust([1])
+        result_emp = binary_heap.up_adjust([])
+        result_None = binary_heap.up_adjust(None)
         
         self.assertListEqual(result_up1,[0,1,3,2,5,6,7,4])
         self.assertListEqual(result_up2,[0,2,1,4,5,6,3])
@@ -42,11 +42,11 @@ class BheapTestCase(unittest.TestCase):
         self.assertEqual(result_None,None)
     def test_down_adjust(self):
         """测试下调操作"""
-        result_down1 = binary_heap.down_adjust(self.array_down1,0)
-        result_down2 = binary_heap.down_adjust(self.array_down2,0)
-        result_one_element = binary_heap.down_adjust(self.array_one_element,0)
-        result_emp = binary_heap.down_adjust(self.array_emp,0)
-        result_None = binary_heap.down_adjust(self.array_None,0)
+        result_down1 = binary_heap.down_adjust([7,0,1,2,3,4,5,6],0)
+        result_down2 = binary_heap.down_adjust([7,1,2,3,4,5,6],0)
+        result_one_element = binary_heap.down_adjust([1],0)
+        result_emp = binary_heap.down_adjust([],0)
+        result_None = binary_heap.down_adjust(None,0)
         
         self.assertListEqual(result_down1,[0, 2, 1, 6, 3, 4, 5, 7])
         self.assertListEqual(result_down2,[1,3,2,7,4,5,6])
@@ -55,10 +55,10 @@ class BheapTestCase(unittest.TestCase):
         self.assertEqual(result_None,None)
     def test_build_heap(self):
         """测试生成二叉堆操作"""
-        result_build = binary_heap.build_heap(self.array_build)
-        result_one_element = binary_heap.build_heap(self.array_one_element)
-        result_emp = binary_heap.build_heap(self.array_emp)
-        result_None = binary_heap.build_heap(self.array_None)
+        result_build = binary_heap.build_heap([4,3,7,0,2,6,5,1])
+        result_one_element = binary_heap.build_heap([1])
+        result_emp = binary_heap.build_heap([])
+        result_None = binary_heap.build_heap(None)
         
         self.assertListEqual(result_build,[0,1,5,3,2,6,7,4])
         self.assertListEqual(result_one_element,[1])
@@ -66,11 +66,11 @@ class BheapTestCase(unittest.TestCase):
         self.assertEqual(result_None,None)  
     def test_delete_element(self):
         """测试删除根节点"""
-        result_delete_element1 = binary_heap.delete_element(self.array_full1)
-        result_delete_element2 = binary_heap.delete_element(self.array_full2)
-        result_one_element = binary_heap.delete_element(self.array_one_element)
-        result_emp = binary_heap.delete_element(self.array_emp)
-        result_None = binary_heap.delete_element(self.array_None)
+        result_delete_element1 = binary_heap.delete_element([0,1,2,3,4,5,6,7])
+        result_delete_element2 = binary_heap.delete_element([0,1,2,3,4,5,6])
+        result_one_element = binary_heap.delete_element([1])
+        result_emp = binary_heap.delete_element([])
+        result_None = binary_heap.delete_element(None)
         
         self.assertListEqual(result_delete_element1,[1,3,2,7,4,5,6])
         self.assertListEqual(result_delete_element2,[1,3,2,6,4,5])
@@ -87,6 +87,10 @@ class BheapTestCase(unittest.TestCase):
         self.assertListEqual(queue_array,[0,3,1,6,4,2,5])
         self.assertListEqual(queue.array,[1,3,2,6,4,5])
         self.assertEqual(pop,0)
+    
+    def test_heap_sort(self):
+        sort_result = binary_heap.heap_sort(list([0,1,2,3,4,5,6,7]))
+        self.assertListEqual(sort_result,list([7,6,5,4,3,2,1,0]))
             
 if __name__ == "__main__":
     unittest.main()
