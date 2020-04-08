c020a1d1232331be5913c882771d483953ab7bd8
cogito0823 <asd754032908@gmail.com>
2020-04-08 22:44:35 +0800

    feat(selection.py): l

--- data_structures/sorting/selection_sort.py
+++ data_structures/sorting/selection_sort.py
@@ -17,7 +17,7 @@ def selection_sort(array):
     >>> selection_sort([7,6,5,4,3,2,1,0])
     [0, 1, 2, 3, 4, 5, 6, 7]
     >>> selection_sort([7,6,5,4,3,2,1])
-    [1, 2, 3, 4, 5, 6, 7 ]
+    [1, 2, 3, 4, 5, 6, 7]
     """
     
     if not array:
