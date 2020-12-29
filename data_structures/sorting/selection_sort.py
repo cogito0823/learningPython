84e8ad85acff93ad7048525d624d7832b8041bee
ccogito_chh <754032908@qq.com>
2020-12-29 21:14:12 +0800

    格式化代码

--- data_structures/sorting/selection_sort.py
+++ data_structures/sorting/selection_sort.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-'''
+"""sdf.
+
 @File    :   selection_sort.py
 @Time    :   2020/03/25 20:31:42
 @Author  :   cogito0823
 @Contact :   754032908@qq.com
 @Desc    :   None
-'''
+"""
+
 
 def selection_sort(array):
-    """选择排序
-    
+    """选择排序.
+
     Examples:
-    
     >>> selection_sort([7,6,5,4,3,2,1,0])
     [0, 1, 2, 3, 4, 5, 6, 7]
     >>> selection_sort([7,6,5,4,3,2,1])
     [1, 2, 3, 4, 5, 6, 7]
+
     """
-    
     if not array:
         return array
     length = len(array)
@@ -30,6 +31,7 @@ def selection_sort(array):
     array[0], array[smallest] = array[smallest], array[0]
     array[1:] = selection_sort(array[1:])
     return array
+
 
 if __name__ == "__main__":
     import doctest
